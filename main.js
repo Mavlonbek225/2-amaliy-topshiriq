62cc56f22e252889ddb44100202d37b1c0352b9f
Mirkamol <a@a.com>
2024-05-03 19:10:26 +0500

    Istalgan sonni so'zga aylantirib beruvchi dastur

---
+++ main.js
@@ -0,0 +1,360 @@
+// 31-dars  Switch  case
+
+// let week = prompt('Hafta kunini kiriting');
+// week = week.toLowerCase();
+// switch (week) {
+//     case 'dushanba':
+//     case 'seshanba':
+//     case 'chorshanba':
+//     case 'payshanba':
+//     case 'juma':
+//         alert ('Ish kuni')
+//         break;
+//     case 'shanba':
+//     case 'yakshanba':
+//         alert ('Dam olish kuni');
+//         break;
+
+//     default:
+//         alert ('Bunday hafta kuni mavjud emas')
+//         break;
+// }
+
+// Vazifa
+// 1-topshiriq
+// let seasons = prompt('Oy nomini kiriting');
+// seasons = seasons.toLowerCase();
+
+// switch (seasons) {
+//     case 'dekabr':
+//     case 'yanvar':
+//     case 'fevral':
+//         alrt ('Qish faslidagi oyning nomi');
+//         break;
+//     case 'mart':
+//     case 'aprel':
+//     case 'may':
+//         alert ('Bahor faslidagi oyning nomi');
+//         break;
+//     case 'iyun':
+//     case 'iyul':
+//     case 'avgust':
+//         alert ('Yoz faslidagi oyning nomi');
+//         break;
+//     case 'sentabr':
+//     case 'oktabr':
+//     case 'noyabr':
+//         alert ('Kuz faslidagi oyning nomi')
+//     break;
+
+//     default:
+//         alert ('Bunday oy nomi mavjud emas')
+//         break;
+// }
+
+// 2-topshiriq
+
+let num = prompt("1 dan 50 gacha bo'lgan istalgan sonni kiriting");
+num = num.toLowerCase();
+switch (num) {
+  case "1":
+    alert("Bir");
+    break;
+  case "2":
+    alert("Ikki");
+    break;
+  case "3":
+    alert("Uch");
+    break;
+  case "4":
+    alert("To'rt");
+    break;
+  case "5":
+    alert("Besh");
+    break;
+  case "6":
+    alert("Olti");
+    break;
+  case "7":
+    alert("Yetti");
+    break;
+  case "8":
+    alert("Sakkiz");
+    break;
+  case "9":
+    alert("To'qqiz");
+    break;
+  case "10":
+    alert("O'n");
+    break;
+  case "11":
+    alert("O'n bir");
+    break;
+  case "12":
+    alert("O'n ikki");
+    break;
+  case "13":
+    alert("O'n uch");
+    break;
+  case "14":
+    alert("O'n to'rt");
+    break;
+  case "15":
+    alert("O'n besh");
+    break;
+  case "16":
+    alert("O'n olti");
+    break;
+  case "17":
+    alert("O'n yetti");
+    break;
+  case "18":
+    alert("O'n sakkiz");
+    break;
+  case "19":
+    alert("O'n to'qqiz");
+    break;
+  case "20":
+    alert("Yigirma");
+    break;
+  case "21":
+    alert("Yigirma bir");
+    break;
+  case "22":
+    alert("Yigirma ikki");
+    break;
+  case "23":
+    alert("Yigirma uch");
+    break;
+  case "24":
+    alert("Yigirma to'rt");
+    break;
+  case "25":
+    alert("Yigirma besh");
+    break;
+  case "26":
+    alert("Yigirma olti");
+    break;
+  case "27":
+    alert("Yigirma yetti");
+    break;
+  case "28":
+    alert("Yigirma sakkiz");
+    break;
+  case "29":
+    alert("Yigirma to'qqiz");
+    break;
+  case "30":
+    alert("O'ttiz");
+    break;
+  case "31":
+    alert("O'ttiz bir");
+    break;
+  case "32":
+    alert("O'ttiz ikki");
+    break;
+  case "33":
+    alert("O'ttiz uch");
+    break;
+  case "34":
+    alert("O'ttiz to'rt");
+    break;
+  case "35":
+    alert("O'ttiz besh");
+    break;
+  case "36":
+    alert("O'ttiz olti");
+    break;
+  case "37":
+    alert("O'ttiz yetti");
+    break;
+  case "38":
+    alert("O'ttiz sakkiz");
+    break;
+  case "39":
+    alert("O'ttiz to'qqiz");
+    break;
+  case "40":
+    alert("Qirq");
+    break;
+  case "41":
+    alert("Qirq bir");
+    break;
+  case "42":
+    alert("Qirq ikki");
+    break;
+  case "43":
+    alert("Qirq uch");
+    break;
+  case "44":
+    alert("Qirq to'rt");
+    break;
+  case "45":
+    alert("Qirq besh");
+    break;
+  case "46":
+    alert("Qirq olti");
+    break;
+  case "47":
+    alert("Qirq yetti");
+    break;
+  case "48":
+    alert("Qirq sakkiz");
+    break;
+  case "49":
+    alert("Qirq to'qqiz");
+    break;
+  case "50":
+    alert("Ellik");
+    break;
+  case "51":
+    alert("Ellik bir");
+    break;
+  case "52":
+    alert("Ellik ikki");
+    break;
+  case "53":
+    alert("Ellik uch");
+    break;
+  case "54":
+    alert("Ellik to'rt");
+    break;
+  case "55":
+    alert("Ellik besh");
+    break;
+  case "56":
+    alert("Ellik olti");
+    break;
+  case "57":
+    alert("Ellik yetti");
+    break;
+  case "58":
+    alert("Ellik sakkiz");
+    break;
+  case "59":
+    alert("Ellik to\'qqiz");
+    break;
+  case "60":
+    alert("Oltmish");
+    break;
+  case "61":
+    alert("Oltmish bir");
+    break;
+  case "62":
+    alert("Oltmish ikki");
+    break;
+  case "63":
+    alert("Oltmish uch");
+    break;
+  case "64":
+    alert("Oltmish to\'rt");
+    break;
+  case "65":
+    alert("Oltmish besh");
+    break;
+  case "66":
+    alert("Oltmish olti");
+    break;
+  case "67":
+    alert("Oltmish yetti");
+    break;
+  case "68":
+    alert("Oltmish sakkiz");
+    break;
+  case "69":
+    alert("Oltmish to\'qqiz");
+    break;
+  case "70":
+    alert("Yetmish");
+    break;
+  case "71":
+    alert("Yetmish bir");
+    break;
+  case "72":
+    alert("Yetmish ikki");
+    break;
+  case "73":
+    alert("Yetmish uch");
+    break;
+  case "74":
+    alert("Yetmish to'rt");
+    break;
+  case "75":
+    alert("Yetmish besh");
+    break;
+  case "76":
+    alert("Yetmish olti");
+    break;
+  case "77":
+    alert("Yetmish yetti");
+    break;
+  case "78":
+    alert("Yetmish sakkiz");
+    break;
+  case "79":
+    alert("Yetmish to\'qqiz");
+    break;
+  case "80":
+    alert("Sakson");
+    break;
+  case "81":
+    alert("Sakson bir");
+    break;
+  case "82":
+    alert("Sakson ikki");
+    break;
+  case "83":
+    alert("Sakson uch");
+    break;
+  case "84":
+    alert("Sakson to\'rt");
+    break;
+  case "85":
+    alert("Sakson besh");
+    break;
+  case "86":
+    alert("Sakson olti");
+    break;
+  case "87":
+    alert("Sakson yetti");
+    break;
+  case "88":
+    alert("Sakson sakkiz");
+    break;
+  case "89":
+    alert("Sakson to\'qqiz");
+    break;
+  case "90":
+    alert("To\'qson");
+    break;
+  case "91":
+    alert("To\'qson bir");
+    break;
+  case "92":
+    alert("To\'qson ikki");
+    break;
+  case "93":
+    alert("To\'qson uch");
+    break;
+  case "94":
+    alert("To\'qson to\'rt");
+    break;
+  case "95":
+    alert("To\'qson besh");
+    break;
+  case "96":
+    alert("To\'qson olti");
+    break;
+  case "97":
+    alert("To\'qson yetti");
+    break;
+  case "98":
+    alert("To\'qson sakkiz");
+    break;
+  case "99":
+    alert("To\'qson to\'qqi");
+    break;
+  case "100":
+    alert("Yuz");
+    break;
+}
